f61f627a9a7d37b8ebbb6f92fb97e7458e838819
写用户名 <why@163.com>
2017-05-18 23:55:21 +0800

    创建了cat类

---
+++ weibo/weibo/cat.h
@@ -0,0 +1,13 @@
+//
+//  cat.h
+//  weibo
+//
+//  Created by Flying on 2017/5/18.
+//  Copyright © 2017年 Flying. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cat : NSObject
+
+@end
---
+++ weibo/weibo/cat.m
@@ -0,0 +1,13 @@
+//
+//  cat.m
+//  weibo
+//
+//  Created by Flying on 2017/5/18.
+//  Copyright © 2017年 Flying. All rights reserved.
+//
+
+#import "cat.h"
+
+@implementation cat
+
+@end
